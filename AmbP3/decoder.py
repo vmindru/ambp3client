d611510051e79dcef052b167bf2c4b2fafd56382
Veaceslav Mindru <Veaceslav.Mindru@sizmek.com>
2019-03-03 10:13:14 -0500

    WIP: added HEADER decoding

--- AmbP3/decoder.py
+++ AmbP3/decoder.py
@@ -33,24 +33,36 @@ class Connection:
             print("{}".format(msg))
             self.socket.close()
             exit(1)
-        data = codecs.encode(data, 'hex')
-        data = self._p3_decode(data)
         return data
 
-    def _p3_decode(self, data):
-        decoded_data = data.decode('ascii')
-        return "{}\n".format(decoded_data)
+
+def bin_data_to_ascii(bin_data):
+    "Converts binary input HEX data into ascii"
+    data = codecs.encode(bin_data, 'hex')
+    decoded_data = data.decode('ascii')
+    return "{}".format(decoded_data)
+
+
+def bin_dict_to_ascii(dict):
+    "takes as input Dict with Binary values, converts into ascii, returns covnerted dict"
+    for key, value in dict.items():
+        dict[key] = bin_data_to_ascii(value)
+    return dict
+
+
+def _byte_reorder():
+    pass
 
 
 def split(data):
     def _get_header(data):
-        str_header = data[0:20]
-        header = {"SOR": str_header[0:2],
-                  "Version": str_header[2:4],
-                  "Length": str_header[4:8][::-1],
-                  "CRC": str_header[8:12][::-1],
-                  "Flags": str_header[12:16][::-1],
-                  "TOR": str_header[16:20][::-1]
+        str_header = data[0:10]
+        header = {"SOR": str_header[0:1],
+                  "Version": str_header[1:2],
+                  "Length": str_header[2:4][::-1],
+                  "CRC": str_header[4:6][::-1],
+                  "Flags": str_header[6:8][::-1],
+                  "TOR": str_header[8:10][::-1]
                   }
         return header
     return _get_header(data)
--- amb_client.py
+++ amb_client.py
@@ -5,6 +5,8 @@ from sys import exit
 from AmbP3.config import Config
 from AmbP3.decoder import Connection
 from AmbP3.decoder import split as decode
+from AmbP3.decoder import bin_data_to_ascii as data_to_ascii
+from AmbP3.decoder import bin_dict_to_ascii as dict_to_ascii
 from AmbP3.write import Write
 
 
@@ -21,8 +23,8 @@ def main():
         with open(config.file, "a") as file_handler:
             while True:
                 data = connection.read()
-                print("Input Data: {}\nDecoded data: {}".format(data, decode(data)))
-                Write.to_file(data, file_handler)
+                print("Input Data: {}\nDecoded data: {}\n".format(data_to_ascii(data), dict_to_ascii(decode(data))))
+                Write.to_file(data_to_ascii(data), file_handler)
                 sleep(0.5)
     except KeyboardInterrupt:
         print("Closing")
